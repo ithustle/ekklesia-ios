433ebecfe6c216ae68d42fbc766db60c048759af
Célio Garcia <kwanzaonline@gmail.com>
2024-09-25 17:15:55 +0100

    Primeiro commit

---
+++ ekklesia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ekklesia
+//
+//  Created by Célio Garcia on 25/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ekklesia/ekklesiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ekklesiaApp.swift
+//  ekklesia
+//
+//  Created by Célio Garcia on 25/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ekklesiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
